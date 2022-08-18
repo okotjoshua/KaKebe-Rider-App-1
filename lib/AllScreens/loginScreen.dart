3187868a31c3176d32e9e6d50f4793d49589d47e
okotjoshua <okotjoshua43@gmail.com>
2022-08-18 08:29:31 +0300

    File Initials

---
+++ lib/AllScreens/loginScreen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding:  EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(height: 35.0,),
+              Image(image: AssetImage("images/logo.png"),
+                width: 390.0,
+                height: 250.0,
+                alignment: Alignment.center,
+              ),
+
+
+              SizedBox(height: 1.0,),
+              Text(
+                "Login as a Rider",
+                style: TextStyle(fontSize: 24.0, fontFamily: "Brand Bold"),
+                textAlign: TextAlign.center,
+              ),
+
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+
+                    SizedBox(height: 1.0,),
+                    TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+
+
+                    SizedBox(height: 1.0,),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+
+                    SizedBox(height: 10.0,),
+                    RaisedButton(
+                      color: Colors.yellow,
+                      textColor: Colors.white,
+                      child: Container(
+                        height: 50.0,
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(fontSize: 18.0, fontFamily: "Brand Bold"),
+                          ),
+                        ),
+                      ),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(24.0),
+                      ),
+                      onPressed: ()
+                      {
+                        print("Joshua Welcome");
+                      },
+                    ),
+
+
+                  ],
+                ),
+              ),
+
+              FlatButton(
+                onPressed: ()
+                {
+                  print("clicked");
+                },
+                child: Text(
+                  "Do not have an Account? Register Here.",
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+
+}
